9a8cc1905a46a72a53b35b10a066e5cc444a8571
Mohamed Akram <mohamedakram392005@gmail.com>
2026-05-08 18:26:23 +0530

    mongo update

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,7 +2,7 @@ import os
 import certifi
 from motor.motor_asyncio import AsyncIOMotorClient
 
-MONGO_URL = os.environ.get("MONGO_URI", "mongodb+srv://akrammohamed392005_db_user:riyaz06042005@cluster2.srig9zh.mongodb.net/?appName=Cluster2")
+MONGO_URL = os.environ.get("MONGO_URI", "mongodb+srv://akram:Akram@123@cluster0.xxxxx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 client = AsyncIOMotorClient(
     MONGO_URL, 
